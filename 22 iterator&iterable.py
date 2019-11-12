1bd59ac2995bad99b8437f92fbd2a303eee98590
imranzahid81 <imranzahid81@hotmail.com>
2019-11-13 04:53:07 +0500

    New Start

--- 22 iterator&iterable.py
+++ 22 iterator&iterable.py
@@ -4,8 +4,8 @@ nums = [1, 2, 3]
 
 i_nums = iter(nums)
 
-# print(i_nums)
-# print(dir(i_nums))
+print(i_nums)
+print(dir(i_nums))
 
 print(f"1st Iterator : {next(i_nums)}")
 print(f"2nd Iterator : {next(i_nums)}")
