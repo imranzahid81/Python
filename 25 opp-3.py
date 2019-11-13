b4c48dca5957897326f15af8c01e88b40c41c91a
imranzahid81 <imranzahid81@hotmail.com>
2019-11-14 03:03:22 +0500

    Beginning Of Second Class Method

--- 25 opp-3.py
+++ 25 opp-3.py
@@ -38,3 +38,37 @@ Employee.set_raise_amt(1.05)
 print(f"All Employees Raise Amount : {Employee.raise_amt}")
 print(f"Employee 1 Raise Amount : {emp_1.raise_amt}")
 print(f"Employee 2 Raise Amount : {emp_2.raise_amt}")
+
+
+# Beginning Of Second Class Method
+
+class Employee01:
+
+    num_of_emps = 0
+    raise_amt = 1.04
+
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = f"{first}.{last}@company.com"
+        Employee01.num_of_emps += 1
+
+    def fullname(self):
+        return(f'Full Name : {self.first} {self.last}')
+
+    # applying raise value by new custom method:
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amt)     # 4 % Raise
+
+    # cls variable is representing class as word class cannot be used......
+    @classmethod
+    def set_raise_amt(cls, amount):
+        cls.raise_amt = amount
+
+
+emp_1 = Employee01('Imran', 'Zahid', 60000)
+emp_2 = Employee01('Talha', 'Hameed', 50000)
+emp_3 = Employee01('Yousuf', 'Qutubuddin', 50000)
+
+
