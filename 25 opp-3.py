e79f8c1dd1b15c06386862cb13bf26bbb7568afb
imranzahid81 <imranzahid81@hotmail.com>
2019-11-14 05:31:46 +0500

    in b/w Of 2nd Class Method B

--- 25 opp-3.py
+++ 25 opp-3.py
@@ -27,20 +27,25 @@ emp_1 = Employee('Imran', 'Zahid', 60000)
 emp_2 = Employee('Talha', 'Hameed', 50000)
 emp_3 = Employee('Yousuf', 'Qutubuddin', 50000)
 
-print(f"Previous Raise Amount : {Employee.raise_amt}")
-print(f"Raised Amount Changed from 1.04 to 1.05")
+# print(f"Previous Raise Amount : {Employee.raise_amt}")
+# print(f"Raised Amount Changed from 1.04 to 1.05")
 Employee.set_raise_amt(1.05)
 
 # But this will apply to all instances as well even if single instance is raised will raise all instances in class example 2 code lines below uncomment to use :
 # print(f"This affects all Instances")
 # emp_1.set_raise_amt(1.06)
 
-print(f"All Employees Raise Amount : {Employee.raise_amt}")
-print(f"Employee 1 Raise Amount : {emp_1.raise_amt}")
-print(f"Employee 2 Raise Amount : {emp_2.raise_amt}")
+# print(f"All Employees Raise Amount : {Employee.raise_amt}")
+# print(f"Employee 1 Raise Amount : {emp_1.raise_amt}")
+# print(f"Employee 2 Raise Amount : {emp_2.raise_amt}")
 
+##############################################################
+##############################################################
+##############################################################
+##############################################################
 
-# Beginning Of Second Class Method
+# Beginning Of 2nd Class Method A
+
 
 class Employee01:
 
@@ -71,4 +76,75 @@ emp_1 = Employee01('Imran', 'Zahid', 60000)
 emp_2 = Employee01('Talha', 'Hameed', 50000)
 emp_3 = Employee01('Yousuf', 'Qutubuddin', 50000)
 
+# Passing in a string to create new Employee :
+# Basic Method :
 
+emp_str_1 = 'John-Doe-70000'
+emp_str_2 = 'Steve-Smith-30000'
+emp_str_3 = 'Jane-Doe-90000'
+
+first, last, pay = emp_str_1.split('-')
+
+new_emp_1 = Employee01(first, last, pay)
+# print(new_emp_1)
+
+# new_emp_1 = Employee.from_string(emp_str_1)
+
+print(new_emp_1.email)
+print(new_emp_1.pay)
+print(Employee01.num_of_emps)
+
+# Beginning Of 2nd Class Method B
+# No need to parse Strings as class method wil do it
+
+
+class Employee02:
+
+    num_of_emps = 0
+    raise_amt = 1.04
+
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = f"{first}.{last}@company.com"
+        Employee02.num_of_emps += 1
+
+    def fullname(self):
+        return(f'Full Name : {self.first} {self.last}')
+
+    # applying raise value by new custom method:
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amt)     # 4 % Raise
+
+    # cls variable is representing class as word class cannot be used......
+    @classmethod
+    def set_raise_amt(cls, amount):
+        cls.raise_amt = amount
+
+    @classmethod
+    def from_string(cls, emp_str):
+        first, last, pay = emp_str.split('-')
+        return cls(first, last, pay)
+        
+
+
+
+emp_1 = Employee02('Imran', 'Zahid', 60000)
+emp_2 = Employee02('Talha', 'Hameed', 50000)
+emp_3 = Employee02('Yousuf', 'Qutubuddin', 50000)
+
+# Passing in a string to create new Employee :
+# Basic Method :
+
+emp_str_1 = 'John-Doe-70000'
+emp_str_2 = 'Steve-Smith-30000'
+emp_str_3 = 'Jane-Doe-90000'
+
+new_emp_1 = Employee02.from_string(emp_str_1)
+new_emp_2 = Employee02.from_string(emp_str_2)
+new_emp_3 = Employee02.from_string(emp_str_3)
+
+print(new_emp_1.email)
+print(new_emp_1.pay)
+print(Employee02.num_of_emps)
